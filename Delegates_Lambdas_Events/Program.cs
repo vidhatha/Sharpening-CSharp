a53e4e4ee2a02154b44d884e0e23d3e4349cb1a4
Vidhatha Vivekananda <vvivekananda@catchpoint.com>
2019-01-02 12:48:16 +0530

    Add project files.

---
+++ Delegates_Lambdas_Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Lambdas_Events
+{
+    class Program
+    {
+        public delegate int MyDelegate(int i, int j);
+
+        public static int add(int i, int j)
+        {
+            return i + j;
+        }
+
+        public static int subtract(int i, int j)
+        {
+            return i - j;
+        }
+        static void Main(string[] args)
+        {
+            int result = 0;
+            MyDelegate fptr = add;
+
+            result = add(10, 20);
+            Console.WriteLine(result);
+
+            result = subtract(10, 20);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
